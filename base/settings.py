c3c1cf594e29a13f9891e37be9dfeaad22e75faf
paul30041981 <paul.cruces@celeritech.biz>
2021-03-28 10:10:01 -0500

    v.1.1

--- base/settings.py
+++ base/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 #Permite que tus apis puedan ser accedidas publicamente
 CORS_ALLOW_ALL_ORIGINS = True
